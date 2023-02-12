c32274394acb5d1692b9ec36c4997b5f5e44a396
Zelamkh4 <zelamkh.tutushev@icloud.com>
2023-02-12 04:29:19 -0800

    Added If/Else

---
+++ C#005.ConditionIFElse/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Added user name");
+string username = Console.ReadLine();
+
+if(username == "Masha");
+{
+    Console.WriteLine("Hooray, It's Masha!");
+}
+
+{
+    Console.Write("Hello, ");
+    Console.WriteLine(username);
+}
