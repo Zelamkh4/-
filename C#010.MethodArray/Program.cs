4c5603b5969ef991e9ccd91d5ac3b1228ce8999b
Zelamkh4 <zelamkh.tutushev@icloud.com>
2023-02-22 10:03:48 -0800

    added method

---
+++ C#010.MethodArray/Program.cs
@@ -0,0 +1,18 @@
+﻿int[] array = {12, 13, 44, 1111, 55, 11, 33, 443, 1111,};
+
+int n = array.Length;
+int find = 1111;
+
+int index = 0;
+
+while (index < n)
+{
+    
+    if(array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    //index = index + 1;
+    index++;
+}
